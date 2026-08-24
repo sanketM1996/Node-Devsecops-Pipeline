ccb419cd218ae5a9011a02bbf9f70057f36934ac
sanketM1996 <sanketmahajan2496@gmail.com>
2026-08-24 18:37:10 +0530

    olde required version

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,15 @@
-import js from "@eslint/js";
 import globals from "globals";
-import { defineConfig } from "eslint/config";
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-]);
+export default [
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "commonjs",
+      globals: globals.node
+    },
+    rules: {
+      "no-unused-vars": "warn"
+    }
+  }
+];
